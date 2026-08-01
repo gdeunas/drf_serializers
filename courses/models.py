fe16bc39cef975a8516be7bd99eab37ab5c45bb7
aidar <anged@mail.ru>
2026-08-01 22:59:25 +0300

    add CRUD

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название курса')
+    preview = models.ImageField(upload_to='courses/courses/', verbose_name='Превью (картинка)', blank=True, null=True)
+    description = models.TextField(verbose_name='Описание курса', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons', verbose_name='Курс')
+    title = models.CharField(max_length=150, verbose_name='Название урока')
+    description = models.TextField(verbose_name='Описание урока', blank=True, null=True)
+    preview = models.ImageField(upload_to='courses/lessons/', verbose_name='Превью (картинка)', blank=True, null=True)
+    video_url = models.URLField(verbose_name='Ссылка на видео', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+
+    def __str__(self):
+        return self.title
--- users/models.py
+++ users/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Email", help_text="Email address"
+    )
+    phone = models.CharField(
+        max_length=11,
+        blank=True,
+        null=True,
+        verbose_name="Phone",
+        help_text="Phone number",
+    )
+    city = models.CharField(
+        max_length=11, blank=True, null=True, verbose_name="City", help_text="City"
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars/",
+        blank=True,
+        null=True,
+        verbose_name="Avatar",
+        help_text="Avatar",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.email
+
