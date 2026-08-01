0c3ffe6a31b8a5499519e91f90761999964cda42
aidar <anged@mail.ru>
2026-08-01 23:05:02 +0300

    add serializers for users

---
+++ users/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from users.models import User
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'phone', 'city', 'avatar', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True, 'required': False},
+            'email': {'required': False}
+        }
+
+    def update(self, instance: User, validated_data):  # Added type hint here
+        password = validated_data.pop('password', None)
+        if password:
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from users.models import User
+from users.serializers import UserProfileSerializer
 
-# Create your views here.
+class UserProfileUpdateAPIView(generics.RetrieveUpdateAPIView):
+    """
+    Эндпоинт для просмотра и редактирования профиля пользователя.
+    Принимает PUT и PATCH запросы для обновления по id.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserProfileSerializer
